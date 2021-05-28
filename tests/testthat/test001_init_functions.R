a60357a510227507cd93406e934a622a2ab8419c
BOLR (Bo Larsen) <BOLR@novonordisk.com>
2021-05-28 14:57:06 +0200

    Save works - started to create test cases

---
+++ tests/testthat/test001_init_functions.R
@@ -0,0 +1,35 @@
+
+
+test_that('DB token',
+  {
+    # Open database and verify created DB token
+    db <- initEnvironment(dbType='sqlite',
+                          dbPath='../data/test_db.db')
+    expect_equal(db$dbType, 'sqlite')
+    expect_equal(class(db$dbHandle)[1], 'SQLiteConnection')
+    expect_equal(db$dbHandle@dbname, normalizePath('../data/test_db.db'))
+    expect_equal(db$dbSchema, '')
+    expect_equal(capture.output(str(db$genericQuery)),
+                 capture.output(str(genericQuery_sqlite)))
+    expect_equal(capture.output(str(db$disconnectDB)),
+                 capture.output(str(disconnectDB_sqlite)))
+    expect_equal(capture.output(str(db$dbExistsTable)),
+                 capture.output(str(dbExistsTable_sqlite)))
+    expect_equal(capture.output(str(db$dbExistsTable)),
+                 capture.output(str(dbExistsTable_sqlite)))
+    disconnectDB(db)
+  }
+)
+
+test_that('Error handling',
+  {
+    expect_error(initEnvironment(dbType='sqlite',
+                                 dbPath='../data/none.db'),
+                 regexp = 'database file .+ could not be found')
+    expect_error(initEnvironment(dbType='sqlite',
+                                 dbPath='../data/dummy.db',
+                                 dbCreate = TRUE),
+                 regexp = 'database file .+ exists.')
+  }
+)
+
---
+++ tests/testthat/test021_getStudiesSTSTDTC.R
@@ -0,0 +1,54 @@
+
+# rootPath <- '//FSDKHQ001/dep402$/000-4284/DATA MANAGEMENT/@Data Science/BioCelerate/sendigR/tests/data/'
+
+rootPath <-  '../data/'
+
+
+db <- initEnvironment(dbType='sqlite',
+                      dbPath=normalizePath(paste0(rootPath, 'test_db.db')))
+
+test_that('01 - No input study list, no filtering',
+  {
+    expect_equal(data.table::setorder(getStudiesSTSTDTC(db), 'STUDYID'),
+                 data.table::setorder(data.table::as.data.table(readxl::read_xls(normalizePath(paste0(rootPath, 'expected_getStudiesSTSTDTC.xls')),
+                                                                                 sheet='expect01'))[,NOT_VALID_MSG := as.character(NOT_VALID_MSG) ],
+                                      'STUDYID'))
+
+    expect_equal(data.table::setorder(getStudiesSTSTDTC(db,noFilterReportUncertain = FALSE),
+                                      'STUDYID'),
+                 data.table::setorder(data.table::as.data.table(readxl::read_xls(normalizePath(paste0(rootPath, 'expected_getStudiesSTSTDTC.xls')),
+                                                                                 sheet='expect01'))[,NOT_VALID_MSG := NULL ],
+                                      'STUDYID'))
+    })
+
+test_that('02 - No input study list, start day filter, open end',
+  {
+    expect_equal(data.table::setorder(getStudiesSTSTDTC(db,...), 'STUDYID'),
+                 data.table::setorder(data.table::as.data.table(readxl::read_xls(normalizePath(paste0(rootPath, 'expected_getStudiesSTSTDTC.xls')),
+                                                                                 sheet='expect02')),
+                                      'STUDYID'))
+
+
+  })
+
+test_that('03 - No input study list, end day filter, open start',
+  {
+    expect_equal(data.table::setorder(getStudiesSTSTDTC(db,...), 'STUDYID'),
+                 data.table::setorder(data.table::as.data.table(readxl::read_xls(normalizePath(paste0(rootPath, 'expected_getStudiesSTSTDTC.xls')),
+                                                                                 sheet='expect03')),
+                                      'STUDYID'))
+
+
+  })
+
+test_that('04 - No input study list, start and end day filters',
+  {
+    expect_equal(data.table::setorder(getStudiesSTSTDTC(db,...), 'STUDYID'),
+                 data.table::setorder(data.table::as.data.table(readxl::read_xls(normalizePath(paste0(rootPath, 'expected_getStudiesSTSTDTC.xls')),
+                                                                                 sheet='expect04')),
+                                      'STUDYID'))
+
+
+  })
+
+disconnectDB(db)
