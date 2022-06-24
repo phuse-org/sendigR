7d57f8481571ba6a7fef6abca1552685263f85f8
Yousuf Ali <yousuf.pharma@gmail.com>
2022-06-24 12:44:46 -0400

    database updated

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 pkgload::load_all()
 #need to figure out how to add database
 # path <- system.file(".", package = "sendigR")
-path <- "tests/data/test_db.db"
+path <- "tests/data/send_no_duplicates.db"
 
 db <- sendigR::initEnvironment("sqlite", path)
 sendigR::execSendDashboard(db)
