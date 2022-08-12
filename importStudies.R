9115fd3a163cbbbcdfff1fd997b23bba47ac2a68
wangw106 <Wenxian.wang@bms.com>
2022-08-12 17:51:58 -0400

    write to tempdir() when needed #19
    write to tempdir() when needed #19

--- importStudies.R
+++ importStudies.R
@@ -4,31 +4,34 @@ baseDir <- "C:/BioCelerate"
 studyRoot <- paste(baseDir,'TDSStudies',sep='/')
 dbRoot <- "C:/BioCelerate/SQLLite_DB/"
 
-# set the working directory
+# CT files folder
 pathBase <-"C:/Project/src/R/sendigRPkg"
-if (!is.null(pathBase)) setwd(pathBase)
+
+# Output to temp folder
+tempDir <- tempdir()
+print(tempDir)
 
 # Call the gen_vocab function with the CDISC CT file, extensible CT file and the target json file
-infile2 <- "SEND_Terminology_EXTENSIBLE.txt"
-infile1 <- "SEND Terminology_2021_12_17.txt"
-jsonfile <- "SEND3.1_test.json"
+infile2 <- paste0(pathBase, "/SEND_Terminology_EXTENSIBLE.txt")
+infile1 <- paste0(pathBase, "/SEND Terminology_2021_12_17.txt")
+jsonfile <- paste0(tempDir, "/SEND3.1_test.json")
 sendigR::gen_vocab(list(infile1, infile2),jsonfile)
 
 # Call the standardize_file function to harmonize the xpt file.
 rawXptFolder <- "C:/BioCelerate/TDSStudies/96298/"
-cleanXptFolder <- "C:/BioCelerate/TDSStudiesClean/96298/"
+cleanXptFolder <- paste0(tempDir, "/Studies/96298/")
 sendigR::standardize_file(rawXptFolder, cleanXptFolder, jsonfile )
 
 rawXptFolder <- "C:/BioCelerate/TDSStudies/35449/"
-cleanXptFolder <- "C:/BioCelerate/TDSStudiesClean/35449/"
+cleanXptFolder <- paste0(tempDir, "/Studies/35449/")
 sendigR::standardize_file(rawXptFolder, cleanXptFolder, jsonfile )
 
 rawXptFolder <- "C:/BioCelerate/TDSStudies/43066/"
-cleanXptFolder <- "C:/BioCelerate/TDSStudiesClean/43066/"
+cleanXptFolder <- paste0(tempDir, "/Studies/43066/")
 sendigR::standardize_file(rawXptFolder, cleanXptFolder, jsonfile )
 
 rawXptFolder <- "C:/BioCelerate/TDSStudies/87497/"
-cleanXptFolder <- "C:/BioCelerate/TDSStudiesClean/87497/"
+cleanXptFolder <- paste0(tempDir, "/Studies/87497/")
 sendigR::standardize_file(rawXptFolder, cleanXptFolder, jsonfile )
 
 # xpt clean was done, start to import studies in cleaXptFolder to the sqllite database
@@ -36,12 +39,12 @@ sendigR::standardize_file(rawXptFolder, cleanXptFolder, jsonfile )
 # Initialization
 dbToken <- sendigR::initEnvironment(dbType='sqlite',
                            dbPath=paste(dbRoot,'tds_study_sub.db',sep='/'),
-                           dbCreate=TRUE)
+                           dbCreate=FALSE)
 # Create database schema
-dbCreateSchema(dbToken)
+#dbCreateSchema(dbToken)
 
 # import studies
-cleanXptFolder <- "C:/BioCelerate/TDSStudiesClean"
+cleanXptFolder <- paste0(tempDir, "/Studies")
 xptfolders <- list.dirs(cleanXptFolder, recursive=FALSE)
 for (oneStudy in xptfolders){
    print(oneStudy);
