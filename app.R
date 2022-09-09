7acb169aac2289d735b04626a29dfd0d53a91145
Yousuf Ali <yousuf.pharma@gmail.com>
2022-09-09 12:39:29 -0400

    file was deleted by mistake

---
+++ app.R
@@ -0,0 +1,7 @@
+pkgload::load_all()
+#need to figure out how to add database
+# path <- system.file(".", package = "sendigR")
+path <- "tests/data/send_no_duplicates.db"
+
+db <- sendigR::initEnvironment("sqlite", path)
+sendigR::execSendDashboard(db)
